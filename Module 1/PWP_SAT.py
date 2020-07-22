5d382f7263a632b6ade2c97501a03deab9535758
Ella Johnsen <47972861+ellafj@users.noreply.github.com>
2020-07-22 21:13:54 +0200

    fixed once and cleaned up a bit
    still won't work for 12x12

--- Module 1/PWP_SAT.py
+++ Module 1/PWP_SAT.py
@@ -41,6 +41,7 @@ def SAT(h, w, nPres, dims):
 
     placementConst = []         # Constraint for the potential placements of the presents on the grid
     overlapConst = []
+    onceConst = []
 
     for present in range(nPres):
         dim = dims[present]
@@ -52,6 +53,7 @@ def SAT(h, w, nPres, dims):
 
         temp = []
 
+        # Constraint for connecting gridpoints together for shapes of presents
         for x in range(remainingW+1):
             for y in range(remainingH+1):
                 place = []          # Resetting placements as we begin with new present
@@ -63,53 +65,31 @@ def SAT(h, w, nPres, dims):
 
                 # Or-command as we only need *one* placement of present
                 temp.append(And(*place))
-        #print(placementConst)
+
         placementConst.append(Or(*temp))
-       #here
-        """
-        # Constraint to make sure present is only placed once
-        onceConst = []
-        n = len(placementConst)
-        for i in range(n):
-            for j in range(i):
-                onceConst.append(Not(*[And(placementConst[i], placementConst[j])]))
-
-            
-            if n != 0:
-                print('n', n)
-                onceConst.append(And(placementConst[i], placementConst[i+n-1]))
-                n -= 1
-        if len(onceConst) == 1:
-            s.add(onceConst)
-            print('once1', onceConst, '\n')
-        else:
-            s.add(And(*onceConst))
-            print('once2', (And(*onceConst)), '\n')"""
-
 
         # Initializing constraint that ensures that presents do not overlap
         for x in range(w):
             for y in range(h):
                 for nextPres in range(present):
-                    #oconst = Not(Or(*[And(grid[present][x][y], grid[nextPres][x][y])]))
-                    #s.add(oconst)
-                    #print(oconst)
                     overlapConst.append(Not(*[And([grid[present][y][x], grid[nextPres][y][x]])]))
-                    #overlapConst.append(Not(Or(*[And([grid[present][y][x], grid[nextPres][y][x]])])))
-                    #s.add(Not(Or(*[And([grid[present][y][x], grid[nextPres][y][x]])])))
-                    #print('0', Not(Or(*[And([grid[present][y][x], grid[nextPres][y][x]])])))
-        #if present != 0:
-            #s.add(overlapConst)
-            #s.add(And(*(overlapConst)))
-            #print('overlap',And(*(overlapConst)), '\n')
 
-    s.add(And(*placementConst))           # Adding constraint to the solver
+        # Constraint to make sure present is only placed once
+        n = len(placementConst)
+        for i in range(n):
+            for j in range(i):
+                onceConst.append(*[And(placementConst[i], placementConst[j])])
+
+    # Adding constraints to the solver
+    s.add(And(*placementConst))
+    s.add(And(*overlapConst))
+    s.add(And(*onceConst))
+
     print('where', And(*placementConst), '\n')
-    s.add(And(*(overlapConst)))
-    print('overlap',And(*(overlapConst)), '\n')
+    print('overlap',And(*overlapConst), '\n')
+    print('once', And(*onceConst), '\n')
 
     print('hello')
-    #print(s)
     print(s.check())
     print(s.unsat_core())
     m = s.model()
@@ -167,7 +147,7 @@ def printPaper(w, h, dist):
 
 
 if __name__ == '__main__':
-    w, h, nPres, dims = readFile('./Instances/f12x12.txt')
+    w, h, nPres, dims = readFile('./Instances/3x3.txt')
     print(w,h,nPres,dims)
     m, grid, intGrid = SAT(w, h, nPres, dims)
     dist = collectSolution(m, grid, intGrid)
